d359b877440b667f8ef67505e75fb34ec834bab3
neils-realtair <neilvillareal@gmail.com>
2021-09-11 22:13:26 +0800

    Commence coding activities...have fun!

---
+++ DataStructureActivities/Week1.Test/DigitsTest.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week1.Test
+{
+    [TestClass]
+    public class DigitsTest
+    {
+        [DataTestMethod]
+        [DataRow(29,11)]
+        [DataRow(123,6)]
+        [DataRow(527,14)]
+        [DataRow(123456,21)]
+        public void AddDigits_CheckIfSum_Correct(int n, int expectedSum)
+        {
+            Assert.AreEqual(expectedSum, Digits.AddDigits(n));
+        }
+
+        [DataTestMethod]
+        [DataRow(2, 99)]
+        [DataRow(3, 999)]
+        [DataRow(4, 9999)]
+        [DataRow(5, 99999)]
+        public void LargestNumber_Check_If_Correct(int n, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Digits.LargestNumber(n));
+        }
+
+        [DataTestMethod]
+        [DataRow(3, 10, 9)]
+        [DataRow(1, 2, 2)]
+        [DataRow(10, 5, 0)]
+        [DataRow(4, 4, 4)]
+        [DataRow(4, 15, 12)]
+        public void OptimalEqualSplit_Check_If_Correct(int n, int m, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Digits.OptimalEqualSplit(n,m));
+        }
+
+        [DataTestMethod]
+        [DataRow(39, 3)]
+        [DataRow(4, 0)]
+        [DataRow(25,2)]
+        [DataRow(999, 4)]
+        public void Persistence_Check_If_Correct(int n, int expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Digits.Persistence(n));
+        }
+
+
+    }
+}
---
+++ DataStructureActivities/Week1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Week1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Week 1 activity!");
+        }
+    }
+
+    public class Digits
+    {
+        /// <summary>
+        /// This function accepts an integer and returns the sum of all its digits
+        /// </summary>
+        /// <param name="n"> n > -1 ||</param>
+        /// <returns></returns>
+        public static int AddDigits(int n)
+        {
+            // todo: do your magic here! :)
+
+            return 0;
+        }
+
+        public static int LargestNumber(int n)
+        {
+            // todo: do your magic here! :)
+
+            return 0;
+        }
+
+        public static int OptimalEqualSplit(int n, int m)
+        {
+            // todo: do your magic here! :)
+
+            return 0;
+        }
+
+        public static int Persistence(int n)
+        {
+            // todo: do your magic here! :)
+
+            return 0;
+        }
+    }
+}
