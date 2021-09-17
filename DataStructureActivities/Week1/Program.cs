d55d8c0b54655087410fdec477a664eb11e09b7a
HiFiona <90251838+HiFiona@users.noreply.github.com>
2021-09-17 16:32:44 +0800

    Update Program.cs

--- DataStructureActivities/Week1/Program.cs
+++ DataStructureActivities/Week1/Program.cs
@@ -20,7 +20,39 @@ namespace Week1
         public static int AddDigits(int n)
         {
             // todo: do your magic here! :)
+ {
+        static void Main(string[] args)
+           {
+            int n = 29, remainder, sum = 0;
+            int n1 = 123, remainder1, sum1 = 0;
+            int n2 = 527, remainder2, sum2 = 0;
+            int n3 = 123456, remainder3, sum3 = 0;
 
+            while( n > 0)
+            while( n1 > 0)
+            while( n2 > 0)
+            while( n3 > 0)
+            {
+                remainder = n % 10;
+                remainder1 = n1 % 10;
+                remainder2 = n2 % 10;
+                remainder3 = n3 % 10;
+
+                sum = sum + remainder;
+                sum1 = sum1 + remainder1;
+                sum2 = sum2 + remainder2;
+                sum3 = sum3 + remainder3;
+
+                n = n / 10;
+                n1 = n1 / 10;
+                n2 = n2 / 10;
+                n3 = n3 / 10;
+            }
+            Console.WriteLine("AddDigits(n) is {0}", sum);
+            Console.WriteLine("AddDigits(n) is {0}", sum1);
+            Console.WriteLine("AddDigits(n) is {0}", sum2);
+            Console.WriteLine("AddDigits(n) is {0}", sum3);
+        }
             return 0;
         }
 
