fadd236b1fc7eb8860f177fbe73b8bb79b39176e
neilvillareal <neilvillareal@gmail.com>
2021-09-17 19:53:45 +0800

    Refactored and reverted all

---
+++ csharp/Week2.Test/BasicStatisticsTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Week2.Test
+{
+    [TestClass]
+    public class BasicStatisticsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ csharp/Week2/BasicStatistics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Week2
+{
+    public class BasicStatistics
+    {
+        public static decimal GetMean(int[] n)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < n.Length; i++)
+                sum += n[i];
+
+            decimal mean = (decimal)sum / (decimal)n.Length;
+
+            return Math.Round(mean, 2);
+        }
+    }
+}
