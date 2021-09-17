76160272949404071f2bcac6b8faa13baa3cb7f7
neilvillareal <neilvillareal@gmail.com>
2021-09-17 19:37:55 +0800

    Revert "Answered MP1 and added Java"
    This reverts commit 18a2037e5c1346c4ece3baaca17eb5bbe38fa411.

--- DataStructureActivities/Week1/Program.cs
+++ DataStructureActivities/Week1/Program.cs
@@ -7,9 +7,6 @@ namespace Week1
         static void Main(string[] args)
         {
             Console.WriteLine("Week 1 activity!");
-            int enteredNumber = int.Parse(Console.ReadLine());
-            int number = Digits.AddDigits(enteredNumber);
-            Console.WriteLine($"Result: {number}");
         }
     }
 
@@ -22,30 +19,16 @@ namespace Week1
         /// <returns></returns>
         public static int AddDigits(int n)
         {
-            var digits = n.ToString().ToCharArray();
+            // todo: do your magic here! :)
 
-            int sum = 0;
-
-            for (int i = 0; i < digits.Length; i++)
-            {
-                // 123 = 6  /// digits [1][2][3]
-                // indices:             0  1  2
-                sum = sum + int.Parse(digits[i].ToString());
-            }       
-
-            return sum;
+            return 0;
         }
 
         public static int LargestNumber(int n)
-        {  
-            /*var num = Math.Pow(10, n) - 1;
-            return (int)num;*/
+        {
+            // todo: do your magic here! :)
 
-            string highnumber = "";
-            for (int i = 0; i < n; i++)
-                highnumber += "9";
-            
-            return int.Parse(highnumber);
+            return 0;
         }
 
         public static int OptimalEqualSplit(int n, int m)
@@ -63,5 +46,3 @@ namespace Week1
         }
     }
 }
-
-
